47d46b74abd69258131f205587d53c1bf83fe112
radioactiveshrimp <radioactiveshrimpmail@gmail.com>
2025-10-26 23:07:59 -0500

    update __init__.py for model

--- src/radioactiveshrimp/model/__init__.py
+++ src/radioactiveshrimp/model/__init__.py
@@ -1,5 +1,8 @@
 """
 Model subpackage containing model functions.
 """
-from .regression import LinearRegression, LogisticRegression
-__all__=['LinearRegression,LogisticRegression']
+from .regression import LinearRegression, CauchyModel, CauchyRegression
+__all__=['LinearRegression,CauchyModel,CauchyRegression']
+
+from .logit import LogisticRegression
+__all__=['LogisticRegression']
